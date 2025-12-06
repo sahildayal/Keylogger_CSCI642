adfe3b78e8a76a031b26bebefdd09960390c7e84
Jared Randall <113712056+jcr8969@users.noreply.github.com>
2025-12-05 21:25:11 -0500

    Delete phase3 directory

--- phase3/utils/run_instances.bat
+++
@@ -1,5 +0,0 @@
-start cmd /k python .\keylogger.py
-start cmd /k python .\keylogger.py
-start cmd /k python .\keylogger.py
-@REM start cmd /k python .\keylogger.py
-@REM start cmd /k python .\keylogger.py
